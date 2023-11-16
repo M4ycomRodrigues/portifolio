5f2b2759e8cf1a82fc013e2e4996a7e9c833f5c5
M4ycomRodrigues <maycomjose70@gmail.com>
2023-11-16 18:34:44 -0300

    Configurando Portifólio

--- js/script.js
+++ js/script.js
@@ -2,10 +2,10 @@ function openNav(){
     var x = document.getElementById("navigation");
 
     if(x.className == "navigation"){
-        x.className += "menuJs";
-        document.getElementById("threeline-icon").innerHTML = "bbbbbbbb";
+        x.className += " menuJs";
+        document.getElementById("threeline-icon").innerHTML = "&Cross;";
     }else{
         x.className = "navigation";
-        document.getElementById("threeline-icon").innerHTML = "#9776;";
+        document.getElementById("threeline-icon").innerHTML = "&#9776;";
     }
 }
